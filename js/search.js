9566bf0658806d4db5be2b36e8a0f70b2a9eab81
NonePointer666 <lgc541800@163.com>
2019-02-20 10:32:00 +0800

    lgc 8 ci

--- js/search.js
+++ js/search.js
@@ -18,31 +18,32 @@
     var requireList=$("#requireList");
     var searchBtn=$("#search_btn");
     var house_list=$(".house_list");
+    var page=$("#page");
     var url_search="http://127.0.0.1:8080/user/search";
 
-var all_room;
+var all_room={};
 // 分页
-pagess(66);
+
 function pagess(b) {
     var ul=$(".page_box");
-    b=Math.ceil(b / 6);
+    b=Math.ceil(b / 4);
     if (b == 1) {
         ul.innerHTML = `<li class="page_boxs" id="index_page">首页</li>
             <li class="page_boxs" id="previous">上一个</li>
-            <li class="pages">1</li>
+            <li class="pages page_active">1</li>
             <li class="page_boxs" id="next">下一个</li>
             <li class="page_boxs" id="last_page">尾页</li>`
     } else if (b == 2) {
         ul.innerHTML = `<li class="page_boxs" id="index_page">首页</li>
             <li class="page_boxs" id="previous">上一个</li>
-            <li class="pages">1</li>
+            <li class="pages page_active">1</li>
             <li class="pages">2</li>
             <li class="page_boxs" id="next">下一个</li>
             <li class="page_boxs" id="last_page">尾页</li>`
     } else if (b >= 3) {
         ul.innerHTML = `<li class="page_boxs" id="index_page">首页</li>
             <li class="page_boxs" id="previous">上一个</li>
-            <li class="pages">1</li>
+            <li class="pages page_active">1</li>
             <li class="pages">2</li>
             <li class="pages">3</li>
             <li class="page_boxs" id="next">下一个</li>
@@ -60,6 +61,9 @@ function pagess(b) {
             y.innerText=t;
             t+=1;
         }
+        page.value=1;
+        var p=all_room.slice((page.value-1)*4,page.value*4);
+        renovate(p);
     }
     // 尾页
     last_page.onclick=function () {
@@ -68,46 +72,78 @@ function pagess(b) {
             previous.nextElementSibling.nextElementSibling.innerText=b-1;
             previous.nextElementSibling.nextElementSibling.nextElementSibling.innerText=b;
         }
+        page.value=b;
+        var p=all_room.slice((page.value-1)*4,page.value*4);
+        renovate(p);
     }
     // 上一个
     previous.onclick=function () {
         var a=previous.nextElementSibling.innerText;
-        if (a>1 && b>3){
-            previous.nextElementSibling.innerText=a-1;
-            previous.nextElementSibling.nextElementSibling.innerText=a;
-            previous.nextElementSibling.nextElementSibling.nextElementSibling.innerText=parseInt(a)+1;
-        }else if (a>6){
-            previous.nextElementSibling.innerText=a-5;
-            previous.nextElementSibling.nextElementSibling.innerText=a-4;
-            previous.nextElementSibling.nextElementSibling.nextElementSibling.innerText=a-3;
+        if (a>1){
+            page.value=page.value-1;
+            var p=all_room.slice((page.value-1)*4,page.value*4);
+            renovate(p);
+            if (b>3){
+                previous.nextElementSibling.innerText=a-1;
+                previous.nextElementSibling.nextElementSibling.innerText=a;
+                previous.nextElementSibling.nextElementSibling.nextElementSibling.innerText=parseInt(a)+1;
+            }
+        }else if(a==1){
+            if (page.value>1){
+                page.value=page.value-1;
+            }
+            var p=all_room.slice((page.value-1)*4,page.value*4);
+            renovate(p);
         }
     };
     // 下一个
     next.onclick=function () {
         var a=next.previousElementSibling.innerText;
-        if (b-a<3 && b>3){
-            next.previousElementSibling.previousElementSibling.previousElementSibling.innerText=b-2;
-            next.previousElementSibling.previousElementSibling.innerText=b-1;
-            next.previousElementSibling.innerText=b;
-        }else if (b-a>=3){
-            next.previousElementSibling.previousElementSibling.previousElementSibling.innerText=parseInt(a)+1;
-            next.previousElementSibling.previousElementSibling.innerText=parseInt(a)+2;
-            next.previousElementSibling.innerText=parseInt(a)+3;
+        if (b-a>0){
+            page.value=parseInt(page.value)+1;
+            var p=all_room.slice((page.value-1)*4,page.value*4);
+            renovate(p);
+            if (b>3){
+                next.previousElementSibling.previousElementSibling.previousElementSibling.innerText=a-1;
+                next.previousElementSibling.previousElementSibling.innerText=a;
+                next.previousElementSibling.innerText=parseInt(a)+1;
+            }
+        }else if(b==a){
+            if (page.value<b){
+                page.value=parseInt(page.value)+1;
+            }
+            var p=all_room.slice((page.value-1)*4,page.value*4);
+            renovate(p);
         }
+
     };
     // 页码的点击事件
     ul.onclick=function (e) {
-        if(e.target.className=="pages"){
-            var p=all_room.slice((e.target.innerText-1)*6,e.target.innerText);
-            renovate(p);
-        }
+        var page_color=document.querySelectorAll(".pages");
+        if (e.target.nodeName=="LI") {
+            for (var o of ul.children){
+                o.classList.remove("page_active")
+            };
+            if(e.target.className=="pages"){
+
+                page.value=e.target.innerText;
+
+                var p=all_room.slice((page.value-1)*4,page.value*4);
+                renovate(p);
+            };
+            for (var u of page_color){
+                if (u.innerText==page.value){
+                    u.classList.add("page_active");
+                }
+            }
+
+            }
     }
 }
 
 // 刷新内容
     function renovate(res) {
         house_list.innerHTML="";
-        var total=1;
         for (var l of res){
             if (l["rent_style"]==0){
                 l["rentStyle"]="整套出租";
@@ -140,7 +176,7 @@ function pagess(b) {
                         <div class="price_box"><span>¥</span><span class="price">${l["price"]}</span><span>/晚</span></div>
                         <ul class="tag_list">
                             <li>${l["house_style"]}</li>
-                            <li>${l["beds"]}</li>
+                            <li>${l["beds"]}张床铺</li>
                         </ul>
                     </div>
                 </div>
@@ -152,10 +188,6 @@ function pagess(b) {
                     <dd class="name_dd">${l["nick_name"]}</dd>
                 </div>
             </a></div>`
-            total+=1;
-            if (total==6){
-                break;
-            }
         };
         //挑转详情页
         var rooms=document.querySelectorAll(".house_item");
@@ -164,8 +196,6 @@ function pagess(b) {
                 location.href="../pages/details.html";
             }
         }
-        all_room=res;
-
     }
     //搜索
     function search(require){
@@ -180,14 +210,77 @@ function pagess(b) {
                 require[i.getAttribute("id")]=i.value;
             }
         }
-        postData(url_search,require,renovate(res));
+        postData(url_search,require,function (res) {
+            house_list.innerHTML="";
+            all_room=res;
+            res=all_room.slice(0,4);
+            for (var l of res){
+                if (l["rent_style"]==0){
+                    l["rentStyle"]="整套出租";
+                }
+                else if (l["rent_style"]==-1){
+                    l["rentStyle"]="不限";
+                }
+                else if (l["rent_style"]==1){
+                    l["rentStyle"]="独立单间";
+                }
+                else if (l["rent_style"]==2){
+                    l["rentStyle"]="合住房间";
+                }
+                house_list.innerHTML+=`<div class="house_item clearfix">
+                <a class="disblock clearfix" target="_blank" data-room=${l["house_id"]}>
+                <div class="l clearfix">
+                    <div class="img"><img src=${l["img_url1"]}>
+                    </div>
+                    <div class="txt"><h1>${l["room_title"]}</h1>
+                        <h3></h3>
+                        <div class="star_box clearfix"><span>${l["rentStyle"]}/可住${l["number_of_people"]}人</span>
+                            <ul class="stars clearfix" data-val="5">
+                                <li class="star active"></li>
+                                <li class="star active"></li>
+                                <li class="star active"></li>
+                                <li class="star active"></li>
+                                <li class="star active"></li>
+                            </ul>
+                            <span class="score">5分</span></div>
+                        <div class="price_box"><span>¥</span><span class="price">${l["price"]}</span><span>/晚</span></div>
+                        <ul class="tag_list">
+                            <li>${l["house_style"]}</li>
+                            <li>${l["beds"]}张床铺</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="r">
+                    <dt class="headimg"><img
+                            src=${l["icon_url"]}>
+                    </dt>
+                    <dd>房东</dd>
+                    <dd class="name_dd">${l["nick_name"]}</dd>
+                </div>
+            </a></div>`
+            };
+            // 分页
+            pagess(all_room.length);
+            //挑转详情页
+            var rooms=document.querySelectorAll(".house_item");
+            for (var m of rooms){
+                m.onclick=function () {
+                    location.href="../pages/details.html";
+                }
+            }
+
+        });
     }
 
     search(require);
     //点击传值
     rentstyle.onclick=function (e) {
         if (e.target.nodeName=="DIV") {
-            rent_style.value=e.target.getAttribute("data-rent")
+            rent_style.value=e.target.getAttribute("data-rent");
+            for (var w of e.target.parentNode.parentNode.children){
+                w.classList.remove("active");
+            }
+            e.target.parentNode.classList.add("active");
         }
 
     };
